e563a4eb0e410011b31153ef4447a64b85018fa7
Adam Cottrill <adam.cottrill@ontario.ca>
2022-04-01 15:24:29 -0400

    Updated setup.py version 2022.4.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="fwsb-common",
-    version="2022.02.18",
+    version="2022.4.1",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",  # example license
@@ -21,7 +21,7 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Django",
-        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
