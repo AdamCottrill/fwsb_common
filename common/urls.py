f37dbc3772963d399e0111fc572b0d0f0c72ea48
Adam Cottrill <adam.cottrill@ontario.ca>
2022-09-19 11:29:42 -0400

    Added lake, management unit and grid5 detail pages
    this commit adds views, urls, and templates to present available
    information on each entity, including any geometry and associations to
    other common elements.  The grid detail page lists the management
    units that it is part of.  All previously passing tests continue to pass.

--- common/urls.py
+++ common/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path, re_path, include
-from .views import SpeciesList, SpeciesDetail, Grid5List, LakeList, ManagementUnitList
+from .views import (
+    SpeciesList,
+    SpeciesDetail,
+    Grid5List,
+    Grid5Detail,
+    LakeList,
+    LakeDetail,
+    ManagementUnitList,
+    ManagementUnitDetail,
+)
 
 from .api import urls as api_urls
 
@@ -7,9 +16,18 @@ app_name = "common"
 
 urlpatterns = [
     path("lakes", LakeList.as_view(), name="lake_list"),
+    re_path(r"lake/(?P<abbrev>[A-Z]{2})/", LakeDetail.as_view(), name="lake_detail"),
     path("species", SpeciesList.as_view(), name="species_list"),
-    re_path(r"species/(?P<spc>[0-9]{3})/", SpeciesDetail.as_view(), name="species_detail"),
+    re_path(
+        r"species/(?P<spc>[0-9]{3})/", SpeciesDetail.as_view(), name="species_detail"
+    ),
     path("grid5s", Grid5List.as_view(), name="grid5_list"),
+    path("grid5/<slug:slug>/", Grid5Detail.as_view(), name="grid5_detail"),
     path("management_units", ManagementUnitList.as_view(), name="management_unit_list"),
-    path("api/v1/", include(api_urls, namespace="common_api")),    
+    path(
+        "management_unit/<slug:slug>/",
+        ManagementUnitDetail.as_view(),
+        name="management_unit_detail",
+    ),
+    path("api/v1/", include(api_urls, namespace="common_api")),
 ]
--- common/views.py
+++ common/views.py
@@ -6,11 +6,17 @@ from django.views.generic.detail import DetailView
 from .models import Grid5, Lake, ManagementUnit, Species
 
 
+def grouped(l, n):
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+
 class SpeciesList(ListView):
 
     template_name = "common/species_list.html"
     model = Species
     queryset = Species.objects.all()
+
 
 class SpeciesDetail(DetailView):
 
@@ -20,35 +26,89 @@ class SpeciesDetail(DetailView):
     queryset = Species.objects.all()
 
     def get_object(self):
-        return get_object_or_404(Species, spc=self.kwargs['spc'])
-    
+        return get_object_or_404(Species, spc=self.kwargs["spc"])
+
+
 class LakeList(ListView):
 
     template_name = "common/lake_list.html"
     model = Lake
     queryset = Lake.objects.all()
 
+
+class LakeDetail(DetailView):
+
+    template_name = "common/lake_detail.html"
+    model = Lake
+    slug_field = "abbrev"
+    queryset = Lake.objects.all()
+
+    def get_object(self):
+        return get_object_or_404(Lake, abbrev=self.kwargs["abbrev"])
+
+    def get_context_data(self, **kwargs):
+        """Add the list of management units for this lake to our context"""
+        context = super().get_context_data(**kwargs)
+        context["management_units"] = ManagementUnit.objects.filter(
+            lake__abbrev=self.kwargs["abbrev"]
+        ).values("slug", "label", "primary", "mu_type")
+        return context
+
+
 class Grid5List(ListView):
 
     template_name = "common/grid5_list.html"
     model = Grid5
-    queryset = Grid5.objects.prefetch_related('lake').all()
+    queryset = Grid5.objects.prefetch_related("lake").all()
 
-    
+
+class Grid5Detail(DetailView):
+
+    template_name = "common/grid5_detail.html"
+    model = Grid5
+    queryset = Grid5.objects.all()
+
+    def get_context_data(self, **kwargs):
+        """Add the list management units associated with this grid.
+        We add them to the context so that they come back in one query and
+        can have the lake associated them before hand.
+
+        """
+
+        context = super().get_context_data(**kwargs)
+        context["mus"] = ManagementUnit.objects.filter(
+            grids__slug=self.kwargs["slug"]
+        ).prefetch_related("lake")
+
+        return context
+
+
 class ManagementUnitList(ListView):
 
     template_name = "common/management_unit_list.html"
     model = ManagementUnit
-    queryset = ManagementUnit.objects.prefetch_related('lake').all()
-
-    
-    
+    queryset = ManagementUnit.objects.prefetch_related("lake").all()
 
 
-# SpeciesList
-# SpeciesDetail
-# LakeList
-# Grid5List
-# Grid5List
-# ManagementUnitList
-# ManagementUnitDetail
+class ManagementUnitDetail(DetailView):
+
+    template_name = "common/management_unit_detail.html"
+    model = ManagementUnit
+    queryset = ManagementUnit.objects.all()
+
+    def get_context_data(self, **kwargs):
+        """Add the list of grids associated with this management unit.
+        Add them to the context so tha they come back in one query and
+        can be grouped with our helper function.
+
+        """
+        ncol = 6
+        context = super().get_context_data(**kwargs)
+        grids = (
+            Grid5.objects.filter(managementunit__slug=self.kwargs["slug"])
+            .prefetch_related("lake")
+            .order_by("grid")
+        )
+        context["grids"] = grouped(grids, ncol)
+
+        return context
